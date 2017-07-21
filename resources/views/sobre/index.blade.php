fd2932129f7a2336a5a121743934fee0e1c6ddb0
Romario Huebra <romarioj2h@gmail.com>
2017-07-20 22:16:34 -0300

    Buscando somente cursos aprovados

--- resources/views/sobre/index.blade.php
+++ resources/views/sobre/index.blade.php
@@ -12,6 +12,6 @@
         A equipe responsável pelo desenvolvimento desse sistema no momento é só eu mesmo Romário Huebra, mas a esperança é crescer e desenvolver ainda mais esse projeto.
     </p>
     <p class="text-justify">
-        Esse projeto foi desenvolvido com PHP 7.1, Laravel 5.4 e MySQL 5.7.
+        Esse projeto foi desenvolvido com PHP, Laravel e MySQL.
     </p>
 @endsection
