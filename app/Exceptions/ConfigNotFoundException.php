212ca4b83701e5b8e1a90c4f65674a312276da11
Romario Huebra <romarioj2h@gmail.com>
2017-07-23 12:34:03 -0300

    lendo cursos da alura

---
+++ app/Exceptions/ConfigNotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romario
+ * Date: 22/07/17
+ * Time: 17:41
+ */
+
+namespace App\Exceptions;
+
+
+class ConfigNotFoundException extends \Exception
+{
+
+}
